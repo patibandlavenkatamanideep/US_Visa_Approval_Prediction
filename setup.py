1c49723f2b0bef3a6f897943a7088f373c6bc013
Venkata Manideep <venkatamanideep20@gmail.com>
2025-12-26 02:41:26 -0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Manideep",
+    author_email="venkatamanideep20@gmail.com",
+    packages=find_packages(),
+
+)
